03e7f656634a4926cf9b5c1e13d0c982ace4698e
JromoAutomator <jesusromo.llamas@gmail.com>
2019-08-20 12:20:23 -0500

    adding linux

--- com.Framework/src/test/java/com/Keywords/java_Utils.java
+++ com.Framework/src/test/java/com/Keywords/java_Utils.java
@@ -2,37 +2,41 @@ package com.Keywords;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class java_Utils {
 	
-	public WebDriver GetOS(Enums.Browser enBrowser) {
+	public WebDriver getDriver(Enums.Browser enBrowser) {
 		String browserDriver=null;
 		WebDriver driver=null;
 		switch(enBrowser) {
 			case CHROME:
 				browserDriver="webdriver.chrome.driver";
-				switch(System.getProperty("os.name")) {
-				case "Linux":
-					System.setProperty(browserDriver, "./drivers/chromedriver");
-					System.out.println("Running on Linux..");
-					break;
-				case "Mac OS":
-					System.out.println("Running on Mac..");
-					break;
-				default:
-					System.setProperty(browserDriver, "./drivers/chromedriver.exe");
-					System.out.println("Running on Windows..");
-					break;
-				}
+				this.getOS(browserDriver,"./drivers/chromedriver");
+				driver = new ChromeDriver();
 				break;
 			case FIREFOX:
 				browserDriver="webdriver.gecko.driver";
+				this.getOS(browserDriver,"./drivers/geckodriver");
+				driver = new FirefoxDriver();
 				break;
 			case INTERNET_EXPLORER:
 				browserDriver="webdriver.ie.driver";
+				this.getOS(browserDriver,"./drivers/chromedriver");
+				driver = new InternetExplorerDriver();
 				break;
 			case EDGE:
 				browserDriver="webdriver.edge.driver";
+				this.getOS(browserDriver,"./drivers/chromedriver");
+				driver = new EdgeDriver();
+				break;
+			case SAFARI:
+				browserDriver="webdriver.edge.driver";
+				this.getOS(browserDriver,"./drivers/chromedriver");
+				driver = new SafariDriver();
 				break;
 			default:
 				break;
@@ -41,6 +45,22 @@ public class java_Utils {
 		
 		
 		return driver;
+	}
+	
+	private void getOS(String browserDriver,String driverpath) {		
+		switch(System.getProperty("os.name")) {
+			case "Linux":
+				System.setProperty(browserDriver, driverpath);
+				System.out.println("Running on Linux..");
+				break;
+			case "Mac OS":
+				System.out.println("Running on Mac..");
+				break;
+			default:
+				System.setProperty(browserDriver, driverpath+".exe");
+				System.out.println("Running on Windows..");
+				break;
+		}
 	}
 
 }
--- com.Framework/src/test/java/com/RegressionTest/NewTest.java
+++ com.Framework/src/test/java/com/RegressionTest/NewTest.java
@@ -19,24 +19,18 @@ import org.testng.annotations.AfterClass;
 public class NewTest {
 	private WebDriver chromeDriver;
 	private WebDriver firefoxDriver;
-	private Enums.Browser myBrowser = Enums.Browser.CHROME;
 	private java_Utils java_u = new java_Utils();
 
   @BeforeTest(groups = { "Regression" })
   public void beforeSuite() {
-	  this.chromeDriver = java_u.GetOS(myBrowser);
+	  this.chromeDriver = java_u.getDriver(Enums.Browser.CHROME);
 	  this.chromeDriver.manage().window().maximize();
 	  this.chromeDriver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
   }
   
   @BeforeTest(groups = { "SmokeTest" })
   public void beforeSuite2() {
-	  System.out.println(System.getProperty("os.name"));
-	  if(!System.getProperty("os.name").equals("Mac OS X")){
-		  System.setProperty("webdriver.gecko.driver", "Path to .exe");
-		  System.out.println("Running on Windows..");
-	  }
-	  this.firefoxDriver = new FirefoxDriver();
+	  this.firefoxDriver = java_u.getDriver(Enums.Browser.FIREFOX);
 	  this.firefoxDriver.manage().window().maximize();
 	  this.firefoxDriver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
   }
@@ -55,9 +49,9 @@ public class NewTest {
 	  LoginPage.textBox_EnterText(LoginPage.txtEmail, "j2558@gmail.com");
   }
   
-  @Test(groups = { "SmokeTest" },priority=1,description="this is a Test to login into the application",dataProvider = "DataClass.GetdataSource")
-  public void RunwithData(int index,String data) {
-		  System.out.println("index= "+index+" , data = "+data);	
+  @Test(groups = { "SmokeTest" },priority=1,description="this is a Test to login into the application")
+  public void RunwithData() {
+	
   }
   
  
